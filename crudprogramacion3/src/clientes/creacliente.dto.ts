13a50e496287fac57c142228d738a69f628a5972
lucasbarreradev <barreralucas26@gmail.com>
2023-12-18 17:17:48 -0300

    eliminar

--- crudprogramacion3/src/clientes/creacliente.dto.ts
+++
@@ -1,9 +0,0 @@
-export class creaclientedto {
-    Apellido:string
-    Nombre:string
-    Ciudad:string
-    Provincia:string
-    Direccion:string
-    Telefono:string
-    Mail:string
-}
--- crudprogramacion3/src/clientes/modificacliente.dto.ts
+++
@@ -1,9 +0,0 @@
-export class modificaclientedto {
-    Apellido?:string
-    Nombre?:string
-    Ciudad?:string
-    Provincia?:string
-    Direccion?:string
-    Telefono?:string
-    Mail?:string
-}
